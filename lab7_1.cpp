ec6535385f375e05cf3ee0d4c297169abccda936
pawitchaya20 <zeebli35142@gmail.com>
2025-12-15 20:59:01 +0700

    commit

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    int a  = 20;
+    int b = 30;
+    float c = 4;
+    float output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - b/c;
+    cout << "output: " << output;
     return 0;
 }
